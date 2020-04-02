0034b0d1ff2e5050e22f1dd5a98ea55602093b91
MalikaBadaoui <malikamyriambadaoui@gmail.com>
2020-04-02 13:00:07 +0200

    ajout ContactList.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Contact 
-        name = 'Chester Nelson'
-        avatar = 'https://randomuser.me/api/portraits/men/26.jpg'
-        online 
-      />
-      <Contact 
-        name = 'Jayden Owens'
-        avatar = 'https://randomuser.me/api/portraits/men/20.jpg'
-      />
-      <Contact 
-        name = 'Catherine Bailey'
-        avatar = 'https://randomuser.me/api/portraits/women/95.jpg'
-        online  
-      />
+    
+    <div className = "App">
+      <ContactList/>
+
     </div>
   );
 }
