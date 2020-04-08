78f92ddc95e860675eec17e85c5bd76d386d86ca
MalikaBadaoui <malikamyriambadaoui@gmail.com>
2020-04-08 23:38:48 +0200

    création branche et ajoute du state dans contacts.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,21 +2,37 @@ import React from 'react'
 import './Contact.css'
 import PropTypes from 'prop-types';
 
-
-export default function Contact({name, avatar, online}) {
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: props.online,
+        };
+}
+render() {
     return (
         <div className="contact" >
-            <img className="avatar" src={avatar} alt={name} />
-            <div>
-                <p className="name">{name}</p>
-                <div className="status" >
-                    <div className={online ? 'status-online' : 'status-offline'} />
-                    <p className="status-text">{online ? 'online' : 'offline' }</p>
-                </div>
+            <img className="avatar" src={this.props.avatar} alt={this.props.name} />
+        <div>
+            <p className="name">{this.props.name}</p>
+            <div className="status" 
+            onClick={event => {
+                const newOnline = !this.state.online;
+                this.setState({ online: newOnline});
+            }}
+            >
+                <div className={this.props.online ? 'status-online' : 'status-offline'} />
+                <p className="status-text">{this.props.online ? 'online' : 'offline' }</p>
+           
             </div>
         </div>
-    )
-}
+    </div>
+
+    )  
+}   
+}   
+
+export default Contact;
 
 Contact.propTypes = {
     name: PropTypes.string.isRequired,
@@ -24,3 +40,25 @@ Contact.propTypes = {
     online: PropTypes.bool
   };
 
+/*
+ Faire en sorte qu'un clic sur l'indicateur de statut 
+ (ou sur la balise qui englobe l'indicateur et le texte 
+ "Online/Offline") inverse l'attribut online dans le state.
+
+• Faire le rendu conditionnel de l'indicateur de statut 
+ et du texte associé, en fonction non pas de la prop online, 
+  mais de la valeur stockée dans le state.
+
+Comme on n'utilise plus la prop online 
+dans le rendu du composant, tu peux te demander 
+s'il ne faudrait pas tout simplement la supprimer.
+
+On va introduire un "bonus" (optionnel), 
+une petite variante sur tout ce qu'on a fait dans la quête : 
+au lieu d'initialiser la valeur online du state à false 
+dans le constructeur, initialise-la en utilisant props.online. 
+De cette façon, au démarrage de l'application, certains contacts 
+seront directement affichés comme "en ligne" et d'autres non, 
+suivant la valeur de la prop online.
+
+*/
